a78851d03d1c08f54e4932fc3e7f7d2720ea7ec7
TEN  ALEXANDER <35337698+sashaTen@users.noreply.github.com>
2022-09-13 21:13:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+
+/*
+1   you  create   the  file  called   "store"
+2  there   you  create  configure  store and export  default
+3  wrap   your  app  into   provider  of that  store
+4 create   file   called  slice 
+5  in that  file  create  the   createslice({})  with   initial   state  of
+the   datatype   you  need
+ex:  todoSlice = createSlice
+6  export   reducer's   functions   like   increment()   
+7  export  default   todoSlice.reducer
+8 Add Slice Reducers to the Store  like  : 
+export default configureStore({
+  reducer: {
+    todos: todoReducer,
+  },
+});
+
+9    now it  is   time   to  useSelect  (get current  state)  and  usedispatch(set new state)
+
+
+
+
+//////////////////////////////
+SUMMARY   :  
+To sum up, we have 6 steps to implement the Redux Toolkit to our react project:
+
+Install Redux Toolkit and React-Redux Packages
+Create a Redux Store
+Include the Redux Store to App.js parent
+Create a Redux State Slice
+Add Slice Reducers to the Store
+Implementing useSelector and useDispatch in React Components
+*/
 import "./styles.css";
 import React from "react";
 import { useSelector } from "react-redux";
